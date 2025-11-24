7bcdc59d00cc3d5e60864d4da6fc0be9b13e4d56
최정혁 <jeonghyeogc01@gmaile.com>
2025-11-24 11:03:33 +0900

    chore: user 생성 기능 추가

--- src/main/java/com/example/project/user/service/UserService.java
+++ src/main/java/com/example/project/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.example.project.user.service;
 
+import com.example.project.common.entity.User;
+import com.example.project.user.model.request.CreateUserRequest;
+import com.example.project.user.model.response.CreateUserResponse;
 import com.example.project.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,19 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public CreateUserResponse createUser(CreateUserRequest request) {
+
+        User user = new User(
+                request.getUserName(),
+                request.getEmail(),
+                request.getNickname(),
+                request.getPassword()
+        );
+
+        User savedUser = userRepository.save(user);
+
+        return CreateUserResponse.from(savedUser);
+
+    }
 }
