0a0a08ef80356d7b5d3a765e106ac209fb2c2912
oezy-coder <a8609556@gmail.com>
2025-11-24 12:26:35 +0900

    feat: user 수정 기능 추가

--- src/main/java/com/example/project/user/service/UserService.java
+++ src/main/java/com/example/project/user/service/UserService.java
@@ -2,8 +2,10 @@ package com.example.project.user.service;
 
 import com.example.project.common.entity.User;
 import com.example.project.user.model.request.CreateUserRequest;
+import com.example.project.user.model.request.UpdateUserRequest;
 import com.example.project.user.model.response.CreateUserResponse;
 import com.example.project.user.model.response.GetUserResponse;
+import com.example.project.user.model.response.UpdateUserResponse;
 import com.example.project.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -31,11 +33,27 @@ public class UserService {
 
     }
 
+    @Transactional(readOnly = true)
     public GetUserResponse findUser(long userId) {
         User user = userRepository.findById(userId).orElseThrow(
                 () -> new IllegalArgumentException("유저 없음")
         );
 
         return GetUserResponse.from(user);
+    }
+
+    // 내 정보 수정
+    @Transactional
+    public UpdateUserResponse updateUser(UpdateUserRequest request) {
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(
+                () -> new IllegalArgumentException("유저 없음")
+        );
+
+        user.updateUser(
+                request.getNickname(),
+                request.getPassword()
+        );
+
+        return UpdateUserResponse.from(user);
     }
 }
