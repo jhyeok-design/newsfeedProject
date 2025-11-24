7d59f932dbf890eec22aa714a1902a3916ad2969
minss22 <kms141516@gmail.com>
2025-11-24 15:14:37 +0900

    feat: 게시물 단건 조회 기능 추가

--- src/main/java/com/example/project/post/repository/PostRepository.java
+++ src/main/java/com/example/project/post/repository/PostRepository.java
@@ -1,11 +1,11 @@
 package com.example.project.post.repository;
 
-import com.example.project.common.entity.User;
 import com.example.project.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<User,Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByUserIdOrderByCreatedAtDesc(Long userID);
+
 }
