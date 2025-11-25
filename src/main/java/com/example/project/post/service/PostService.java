317910e81085db89b1e4a7bdebde6ef82e788087
minss22 <kms141516@gmail.com>
2025-11-25 17:16:29 +0900

    feat: 게시물 수정/삭제 시 유저가 게시물의 작성자인지 확인하는 메서드 추가 + 예외 처리 추가

--- src/main/java/com/example/project/post/service/PostService.java
+++ src/main/java/com/example/project/post/service/PostService.java
@@ -1,10 +1,7 @@
 package com.example.project.post.service;
 
 import com.example.project.common.entity.User;
-import com.example.project.common.exception.CustomException;
-import com.example.project.common.exception.ErrorCode;
-import com.example.project.common.exception.PostNotFoundException;
-import com.example.project.common.exception.UserNotFoundException;
+import com.example.project.common.exception.*;
 import com.example.project.post.dto.*;
 import com.example.project.post.dto.ReadPostResponse;
 import com.example.project.common.entity.Post;
@@ -12,10 +9,13 @@ import com.example.project.post.repository.PostRepository;
 import com.example.project.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PostService {
 
     private final PostRepository postRepository;
@@ -42,6 +42,7 @@ public class PostService {
      * 게시물 전체 조회
      * @return ReadPostResponse 리스트
      */
+    @Transactional(readOnly = true)
     public List<ReadPostResponse> getAll() {
         List<Post> postList = postRepository.findAllByIsDeletedFalseOrderByCreatedAtDesc();
 
@@ -54,6 +55,7 @@ public class PostService {
      * @param userID 로그인한 유저 ID
      * @return ReadPostResponse 리스트
      */
+    @Transactional(readOnly = true)
     public List<ReadPostResponse> getAllMe(Long userID) {
         // 유저 조회
         User user = userRepository.findById(userID).orElseThrow(UserNotFoundException::new);
@@ -68,25 +70,12 @@ public class PostService {
      * @param postID 조회할 게시물 ID
      * @return ReadPostResponse DTO
      */
+    @Transactional(readOnly = true)
     public ReadPostResponse getOne(Long postID) {
         // 게시물 조회, 삭제 처리된 게시물은 조회 안됨
         Post post = postRepository.findByIdAndIsDeletedFalse(postID).orElseThrow(PostNotFoundException::new);
 
         return ReadPostResponse.from(post);
-    }
-
-    /**
-     * 게시물 삭제 (소프트 삭제)
-     * @param userID 로그인한 유저 ID
-     * @param postID 삭제할 게시물 ID
-     */
-    public void delete(Long userID, Long postID) {
-        // 유저 조회
-        User user = userRepository.findById(userID).orElseThrow(UserNotFoundException::new);
-        // 유저의 게시물 조회, 삭제 처리된 게시물은 조회 안됨
-        Post post = postRepository.findByIdAndUserAndIsDeletedFalse(postID, user).orElseThrow(PostNotFoundException::new);
-
-        post.delete(); // 조회한 게시물 삭제 처리
     }
 
     /**
@@ -96,21 +85,45 @@ public class PostService {
      * @return 게시물 수정 응답 DTO
      */
     public UpdatePostResponse updatePost(Long userID, Long postId, UpdatePostRequest request) {
-        // 유저 조회
-        User user = userRepository.findById(userID).orElseThrow(UserNotFoundException::new);
         // 유저의 게시물 조회, 삭제 처리된 게시물은 조회 안됨
-        Post post = postRepository.findByIdAndUserAndIsDeletedFalse(postId, user).orElseThrow(PostNotFoundException::new);
+        Post post = postRepository.findByIdAndIsDeletedFalse(postId).orElseThrow(PostNotFoundException::new);
 
-        if (request.getTitle().isEmpty() && request.getContent().isEmpty()) {
+        isOwner(userID, post);
+
+        // 아무 정보도 안 줬을 경우
+        if ((request.getTitle() == null || request.getTitle().isEmpty())
+                && (request.getContent() == null || request.getContent().isEmpty()))
             throw new CustomException(ErrorCode.EMPTY_POST_UPDATE);
-        } else if (request.getTitle().isEmpty()) {
-            request.setTitle(post.getTitle());
-        } else if(request.getContent().isEmpty()) {
-            request.setContent(post.getContent());
-        }
 
-        post.updatePost(request);
+        post.update(request);
 
         return UpdatePostResponse.from(post);
     }
+
+    /**
+     * 게시물 삭제 (소프트 삭제)
+     * @param userID 로그인한 유저 ID
+     * @param postID 삭제할 게시물 ID
+     */
+    public void delete(Long userID, Long postID) {
+        // 유저의 게시물 조회, 삭제 처리된 게시물은 조회 안됨
+        Post post = postRepository.findByIdAndIsDeletedFalse(postID).orElseThrow(PostNotFoundException::new);
+
+        isOwner(userID, post);
+
+        post.delete(); // 조회한 게시물 삭제 처리
+    }
+
+    /**
+     * 유저가 게시물의 작성자인지 확인
+     * @param userID 로그인한 유저 ID
+     * @param post 게시물
+     */
+    private void isOwner(Long userID, Post post) {
+        // 유저 조회
+        User user = userRepository.findById(userID).orElseThrow(UserNotFoundException::new);
+        // 유저가 게시물의 작성자가 아니면 예외처리
+        if (!post.getUser().equals(user)) throw new NotResourceOwnerException();
+    }
+
 }
