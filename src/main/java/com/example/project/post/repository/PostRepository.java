a1f1a23207e1cdb7afda81ac457501395d48aec5
minss22 <kms141516@gmail.com>
2025-11-25 13:00:17 +0900

    feat: 내 게시글 조회/수정/삭제 수정 + 예외 처리 추가
    - 내 게시글 조회/수정/삭제 시 유저를 먼저 조회하고, 유저와 게시물 ID를 모두 만족하는 게시물을 조회한다.
    - 커스텀 에러 클래스 추가
     - PostNotFoundException
     - UserNotFoundException

--- src/main/java/com/example/project/post/repository/PostRepository.java
+++ src/main/java/com/example/project/post/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.project.post.repository;
 
+import com.example.project.common.entity.User;
 import com.example.project.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,12 +9,15 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    // 전체 조회, 삭제 처리된 게시물은 조회 안됨, 생성일자 기준으로 내림차순
+    // 전체 게시물 조회, 삭제 처리된 게시물은 조회 안됨, 생성일자 기준으로 내림차순
     List<Post> findAllByIsDeletedFalseOrderByCreatedAtDesc();
 
-    // 유저 ID를 기준으로 전체 조회, 삭제 처리된 게시물은 조회 안됨, 생성일자 기준으로 내림차순
-    List<Post> findByUserIdAndIsDeletedFalseOrderByCreatedAtDesc(Long userID);
+    // 유저의 전체 게시물 조회, 삭제 처리된 게시물은 조회 안됨, 생성일자 기준으로 내림차순
+    List<Post> findByUserAndIsDeletedFalseOrderByCreatedAtDesc(User user);
 
     // 게시물 조회, 삭제 처리된 게시물은 조회 안됨
     Optional<Post> findByIdAndIsDeletedFalse(Long postID);
+
+    // 유저의 게시물 조회, 삭제 처리된 게시물은 조회 안됨
+    Optional<Post> findByIdAndUserAndIsDeletedFalse(Long postID, User user);
 }
