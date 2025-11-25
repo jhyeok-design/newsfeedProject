d2d95036e7cecc276574a179a658da3ee5240615
i-am-dahee <hxxly02@gmail.com>
2025-11-25 19:20:55 +0900

    fix: 비밀번호 검증 조건에 맞게 수정
    - 대문자 + 소문자 + 숫자 + 기호 최소 1개씩 사용
    - 최소 8자 이상

--- src/main/java/com/example/project/user/model/request/DeleteUserRequest.java
+++ src/main/java/com/example/project/user/model/request/DeleteUserRequest.java
@@ -6,8 +6,7 @@ import lombok.Getter;
 
 @Getter
 public class DeleteUserRequest {
-    // 임시방편
+
     @NotBlank(message = "비밀번호는 필수입니다.")
-    @Size(min = 6, message = "비밀번호가 너무 짧습니다.")
     private String password;
 }
