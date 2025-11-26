0c206a2384d62e30d69d43155d6c6abcf6692d2e
oezy-coder <a8609556@gmail.com>
2025-11-26 10:43:51 +0900

    refactor: UserService 내 정보 수정 기능 개선 - User 엔티티에 nullable 컬럼 설정 추가 및 수정 메서드 분리 - 닉네임/비밀번호 개별 수정 가능하도록 updateMe 로직 리팩토링 - ErrorCode (NOTHING_TO_UPDATE, DUPLICATE_NICKNAME 등) 추가 및 적용 - UserRepository에 existsByNicknameAndIdNot 추가

--- src/main/java/com/example/project/common/entity/User.java
+++ src/main/java/com/example/project/common/entity/User.java
@@ -22,19 +22,22 @@ public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
+    @Column(nullable = false)
     private String userName;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String nickname;
 
+    @Column(nullable = false)
     private String password;
 
     // 회원정보 논리적 삭제 플레그
+    @Column(nullable = false)
     private boolean deleted = false;
     // 삭제 시간 기록용
     private LocalDateTime deletedAt;
@@ -50,10 +53,15 @@ public class User extends BaseEntity {
         this.password = password;
     }
 
-    // 유저의 닉네임, 비밀번호 수정
-    public void updateUser(String nickname, String password) {
+    // 유저의 닉네임 수정
+    public void modifyNickname(String nickname) {
         this.nickname = nickname;
-        this.password = password;
+    }
+
+    // 유저의 비밀번호 수정
+    public void modifyPassword(String encodedPassword) {
+        this.password = encodedPassword;
+        this.increaseTokenVersion();
     }
 
     // 회원정보 논리적 삭제 + 시간 기록
