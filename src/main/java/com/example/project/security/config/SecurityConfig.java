7931917462958a0ac38067bdbe31d31da0e5ec37
i-am-dahee <hxxly02@gmail.com>
2025-11-26 16:35:34 +0900

    fix: 댓글 조회 에러 수정

--- src/main/java/com/example/project/security/config/SecurityConfig.java
+++ src/main/java/com/example/project/security/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/users/*", "/posts", "/users/*/posts/*").permitAll()
                         .requestMatchers(HttpMethod.PATCH,  "/users/me").authenticated()
                         .requestMatchers(HttpMethod.DELETE, "/users/me").authenticated()
+                        .requestMatchers(HttpMethod.PATCH,  "/posts/*/comments").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
